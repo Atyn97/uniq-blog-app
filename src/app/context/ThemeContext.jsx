e6142279445ff058ff2cc2f28d6d04a8b4dbe424
Atyn97 <fatin.nooraina97@gmail.com>
2024-01-25 21:35:31 +0100

    define local storage

--- src/app/context/ThemeContext.jsx
+++ src/app/context/ThemeContext.jsx
@@ -6,7 +6,7 @@ export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
   if (typeof window !== undefined) {
-    const value = localStorage.getItem("theme");
+    const value = localStorage ? JSON.parsegetItem("theme") : null;
     return value || "light";
   }
 };
