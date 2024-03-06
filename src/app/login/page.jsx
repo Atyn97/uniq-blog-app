096dcea3f3cbbc3b5c20de3e02df7afe43ffc693
Atyn97 <fatin.nooraina97@gmail.com>
2024-03-06 16:27:25 +0100

    edit login google

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -18,14 +18,7 @@ const LoginPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div
-          className={styles.socialButton}
-          onClick={() =>
-            signIn("google", {
-              callbackUrl: `${process.env.NEXTAUTH_URL}`,
-            })
-          }
-        >
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
           Sign in with Google
         </div>
         <div className={styles.socialButton}>Sign in with Github</div>
